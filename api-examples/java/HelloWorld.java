6bd8235cd3b7d70d2d90e751e3df33b3768d6798
zharben <zharben@volunteermatch.org>
2012-08-29 12:26:28 -0700

    Java API HelloWorld example

---
+++ api-examples/java/HelloWorld.java
@@ -0,0 +1,43 @@
+package org.vm.example;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ */
+public class HelloWorld {
+  private static final Logger log = Logger.getLogger(HelloWorld.class);
+
+  private static final String HELLO_WORLD_METHOD = "helloWorld";
+
+  /** The first parameter to this program should be the name that will be displayed in the HelloWorld call
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    boolean showUsage = false;
+    if (args.length != 3) {
+      showUsage = true;
+    } else {
+      for (String arg : args) {
+        if (arg == null || arg.equals("")) {
+          showUsage = true;
+          break;
+        }
+      }
+    }
+
+    if (showUsage) {
+      throw new IllegalArgumentException("Usage : java org.vm.example.HelloWorld NameToDisplay ApiUsername ApiPassword");
+    }
+
+    // build up a query based on the program arguments
+    String query = "{\"name\" : \"" + args[0] + "\"}";
+
+    VolunteerMatchApiService service = new VolunteerMatchApiService();
+    String result = service.callAPI(HELLO_WORLD_METHOD, query, VolunteerMatchApiService.HTTP_METHOD_GET, args[1], args[2]);
+    System.out.println(result);
+  }
+
+}
---
+++ api-examples/java/VolunteerMatchApiService.java
@@ -0,0 +1,200 @@
+package org.vm.example;
+
+import org.apache.log4j.Logger;
+import org.bouncycastle.util.encoders.Base64;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ */
+public class VolunteerMatchApiService {
+  private static final Logger log = Logger.getLogger(VolunteerMatchApiService.class);
+  private static final DateFormat DATETIME_FORMAT =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+  private static final String CHARSET = "UTF-8";
+  private static final String API_URL = "http://www.volunteermatch.org/api/call";
+
+  public static final String HTTP_METHOD_GET = "GET";
+  /**
+   * Generate a random nonce.
+   *
+   * @return
+   */
+  private byte[] generateNonce() {
+    try {
+      SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+      byte nonce[] = new byte[20];
+      random.nextBytes(nonce);
+      return nonce;
+    } catch (NoSuchAlgorithmException e) {
+      log.error("Failed to generate nonce", e);
+    }
+    return null;
+  }
+
+  /**
+   * Generates a SHA-256 hash of a payload message.
+   *
+   * @param payload
+   * @return
+   */
+  private byte[] sha256(byte[] payload) {
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      digest.reset();
+      return digest.digest(payload);
+    } catch (NoSuchAlgorithmException e) {
+      log.error("Failed to generate SHA256 hash", e);
+    }
+    return null;
+  }
+
+  /**
+   * Given a name and a password, build a WSSECredentials object.
+   * @param accountName
+   * @param password
+   * @return
+   */
+  private WSSECredentials buildWSSECredentials(String accountName, String password) {
+    WSSECredentials wsse= new WSSECredentials();
+    wsse.userName = accountName;
+    byte [] nonce = generateNonce();
+    if (nonce == null) {
+      log.error("Failed to generate nonce");
+      return null;
+    }
+
+    wsse.nonce = new String(Base64.encode(nonce));
+    wsse.timestamp = DATETIME_FORMAT.format(new Date(System.currentTimeMillis()));
+
+    String digestInput = wsse.nonce + wsse.timestamp + password;
+    wsse.passwordDigest = new String(Base64.encode(sha256((digestInput).getBytes())));
+
+    return wsse;
+  }
+
+  /**
+   * Builds an HTTP connection based on some WSSE credentials, a JSON query and some API key information.
+   * @param wsse
+   * @param query
+   * @param httpMethod
+   * @param accountName
+   * @return
+   */
+  private HttpURLConnection buildConnection(WSSECredentials wsse, String query, String httpMethod, String accountName) {
+    try {
+
+      URL url = new URL(API_URL + "?" + query);
+
+      HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+      urlConnection.setRequestMethod(httpMethod);
+      urlConnection.setRequestProperty("Accept-Charset", CHARSET);
+      urlConnection.setRequestProperty("Content-Type", "application/json");
+      urlConnection.setRequestProperty("Authorization", "WSSE profile=\"UsernameToken\"");
+
+      if ("POST".equals(httpMethod)){
+        urlConnection.setDoOutput(true);
+      }
+
+      StringBuilder credentials= new StringBuilder();
+      credentials.append("UsernameToken Username=\"").append(accountName).append("\", ");
+      credentials.append("PasswordDigest=\"").append(wsse.passwordDigest).append("\", ");
+      credentials.append("Nonce=\"").append(wsse.nonce).append("\", ");
+      credentials.append("Created=\"").append(wsse.timestamp).append("\", ");
+
+      if (log.isDebugEnabled())  {
+        log.debug("Sending request with credentials = "+ credentials);
+      }
+
+      //this header is used to authenticate the request
+      urlConnection.setRequestProperty("X-WSSE", credentials.toString());
+
+      return urlConnection;
+    } catch (MalformedURLException mue) {
+      log.error("Failed to build connection to API", mue);
+    } catch (IOException ioe) {
+      log.error("Failed to build connection to API", ioe);
+    }
+
+    return null;
+  }
+
+  /** Call the VolunteerMatch API for the specified method (
+   *
+   * @param apiMethod
+   * @param query
+   * @param httpMethod
+   * @param accountName
+   * @param password
+   * @return
+   */
+  public String callAPI(String apiMethod, String query, String httpMethod, String accountName, String password) {
+    StringBuilder q = new StringBuilder();
+    HttpURLConnection urlConnection = null;
+    InputStream response = null;
+    try {
+      q.append("action=").append(URLEncoder.encode(apiMethod, CHARSET));
+      q.append("&query=").append(URLEncoder.encode(query, CHARSET));
+
+      WSSECredentials wsse = null;
+
+      wsse = buildWSSECredentials(accountName, password);
+
+      urlConnection = buildConnection(wsse, q.toString(), httpMethod, accountName);
+
+      // fire the call
+      response = urlConnection.getInputStream();
+
+      // collect response
+      BufferedReader reader = null;
+      StringBuilder buf = new StringBuilder();
+      reader = new BufferedReader(new InputStreamReader(response, CHARSET));
+
+      // add return code and message
+      buf.append("Code ").append(urlConnection.getResponseCode()).append(" : ").append(urlConnection.getResponseMessage()).append("\n");
+
+      for (String line; (line = reader.readLine()) != null;) {
+        buf.append(line).append("\n");
+      }
+
+      if (reader != null) try {
+        reader.close();
+      } catch (IOException logOrIgnore) {
+        log.error("Failed to close response reader", logOrIgnore);
+      }
+
+      return buf.toString();
+
+    } catch (Exception e) {
+      try {
+        return "Code " + urlConnection.getResponseCode() + " : " + urlConnection.getResponseMessage();
+      } catch (Exception e2) {
+        log.error("An unknown error occurred while processing an API call for method " + apiMethod + ", query "+ query, e2);
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Structure representing a set of WSSE credentials.
+   */
+  class WSSECredentials {
+    public String userName= "";
+    public String passwordDigest= "";
+    public String nonce= "";
+    public String timestamp= "";
+  }
+
+}
